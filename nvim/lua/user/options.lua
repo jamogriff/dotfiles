c85df0533345b321bf450a5907e56dc597cd2077
Jamison Griffith <jamison@zingstudios.com>
2023-01-04 16:14:19 -0700

    Updates fonts; tmux configuration; WIP nvim config

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,29 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmode = 'longest:full,full' -- complete longest common match; allow tabbing to complete results
+
+vim.opt.title = true
+vim.opt.mouse = 'a'
+--" Some basics:
+--	set encoding=utf-8
+--	set number relativenumber 
+--	set noshowmode
+--	syntax on
+--	imap jk <Esc>
+--	imap kj <Esc>
+--
+--" Tabs/spaces:
+--	filetype indent plugin on
+--	set autoindent " Indent next line
+--	set smartindent " Smart-indent curly braces
+--	set showmatch " Show matching parens and braces
