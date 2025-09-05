94abe0a9d5dad200389af559704d26ea4c0c6d07
Jamison Griffith <jamison.griffith@gmail.com>
2025-09-05 09:16:18 -0600

    WIP latex support

--- nvim/lua/user/plugins/vimtex.lua
+++ nvim/lua/user/plugins/vimtex.lua
@@ -1,2 +1,17 @@
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
+
+-- Configure latexmk to use lualatex
+vim.g.vimtex_compiler_latexmk = {
+  build_dir = '',
+  callback = 1,
+  continuous = 1,
+  executable = 'latexmk',
+  options = {
+    '-pdf',
+    '-pdflatex=lualatex',
+    '-file-line-error',
+    '-synctex=1',
+    '-interaction=nonstopmode',
+  },
+}
